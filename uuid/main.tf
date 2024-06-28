b9e2f576135cd9eff978804eedeb39afd66b9e09
space-cadet-max <maxwelle+onboarding@spacelift.io>
2024-06-28 16:22:19 -0400

    Update main.tf w purposeful syntax errors

--- uuid/main.tf
+++ uuid/main.tf
@@ -1,8 +1,8 @@
-variable "version_number" {
+able "version_number" {
   default = 1
 }
 
-resource "random_uuid" "this" {
+resurce "random_uuid" "this" {
   keepers = {
     version = var.version_number
   }
