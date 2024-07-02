31d66801683bdcacf53cc8760c13cb3886103757
space-cadet-max <maxwelle+onboarding@spacelift.io>
2024-07-02 16:13:07 -0400

    Update main.tf

--- pet/main.tf
+++ pet/main.tf
@@ -1,14 +1,14 @@
 variable "version_number" {
-  default = "0.1.9"
+  default = 1
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "example-bucket-${var.version_number}"
-  acl    = "private"
+resource "random_pet" "this" {
+  keepers = {
+    version = var.version_number
+  }
 }
-
-# Introduce a conflict by creating another bucket with the same name
-resource "aws_s3_bucket" "example_conflict" {
-  bucket = "example-bucket-${var.version_number}"
-  acl    = "private"
+resource "random_pet" "this" {
+  keepers = {
+    version = var.version_number
+  }
 }
